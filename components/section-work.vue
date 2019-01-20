935412ad6979d6b938e7ad8b9f4e3d60ca64a6ef
Kananek T <info.dvgamer@gmail.com>
2019-01-20 22:20:29 +0700

    Update section-work.vue

--- components/section-work.vue
+++ components/section-work.vue
@@ -1,28 +1,42 @@
 <template>
-<section id="work">
+<section class="section-wrapper pt-3 pb-3" v-if="work.length > 0">
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-36 col-sm-36">
-        <div class="section-title">
-          <h2 v-text="$store.state.resume.work.title"></h2>
-          <p v-html="$store.state.resume.work.detail"></p>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="section-title"><h2>Work Experience</h2></div>
+      </div>
+      <div class="col-md-9">
+        <div class="row">
+          <div class="col-md-12" v-for="e in work" :key="work.indexOf(e)">
+            <div class="content-item">
+              <small v-text="e.range">April 2011 - April 2013</small>
+              <h3 v-text="e.job">Freelance — Programmer</h3>
+              <h4 v-text="e.work">Phetchaburi</h4>
+              <div class="markdown-body" v-html="markedown(e.description)"></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div v-for="item in $store.state.resume.work.items" class="col-md-9 col-sm-18">
-        <div class="work-thumb">
-          <a :href="item.thumb" class="image-popup"><img :src="item.image" class="img-fluid" :alt="item.title"></a>
-        </div>
-        <h4 v-text="item.name"></h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-36">
-        <adsbygoogle ad-slot="8273331915" ad-format="auto" :ad-style="{ display: 'block' }" />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-36" style="margin-top: 60px; text-align:right;" v-html="$store.state.resume.footer"></div>
     </div>
   </div>
 </section>
 </template>
+<script>
+import marked from 'marked'
+
+export default {
+  props: {
+    work: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    markedown (text) {
+      return marked(text)
+    }
+  }
+}
+</script>
