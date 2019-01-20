24d8698bb1715a993a726f70e6d884c7c53e2bee
Kananek T <info.dvgamer@gmail.com>
2019-01-20 22:20:36 +0700

    Create section-education.vue

---
+++ components/section-education.vue
@@ -0,0 +1,36 @@
+<template>
+<section class="section-education pt-3 pb-3" v-if="education.length > 0">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="section-title"><h2>Education</h2></div>
+      </div>
+
+      <div class="col-md-9">
+        <div class="row">
+          <div class="col-md-6" v-for="e in education" :key="education.indexOf(e)">
+            <div class="content-item">
+              <small v-text="e.range"></small>
+              <h3 v-text="e.major"></h3>
+              <h4 v-text="e.location">University of California</h4>
+              <p v-text="e.branch">United Kingdom, London</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+<script>
+export default {
+  props: {
+    education: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
