c5c46001be967cb9a3e2cf2cf2c0b180e6545d1c
Kananek T <info.dvgamer@gmail.com>
2019-01-20 22:20:34 +0700

    Create section-expertise.vue

---
+++ components/section-expertise.vue
@@ -0,0 +1,46 @@
+<template>
+<section class="expertise-wrapper pt-3 pb-3" v-if="expertise.length > 0">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="section-title">
+          <h2>My Specialities</h2>
+        </div>
+      </div>
+      <div class="col-md-9">
+        <div class="row">
+          <div class="col-md-6" v-for="e in expertise" :key="expertise.indexOf(e)">
+            <div class="expertise-item">
+              <h3 v-text="e.subject"></h3>
+              <p v-html="e.detail"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+<script>
+export default {
+  props: {
+    expertise: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.expertise-item {
+  margin-bottom : 20px;
+}
+
+.expertise-item h3 {
+  text-transform : uppercase;
+  margin-bottom  : 5px;
+}
+</style>
