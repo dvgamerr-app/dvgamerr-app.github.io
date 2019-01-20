999845df37af38cbbd10952feddd874128fa5608
Kananek T <info.dvgamer@gmail.com>
2019-01-20 22:21:11 +0700

    update resume and style.

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,39 @@
 <template>
 <div id="main-wrapper">
-  <section-header/>
+  <section-header :resume="{ fullname, birthday, national, language, job, detail, social }" />
+  <section-coding/>
+  <section-coding-history/>
+  <section-expertise :expertise="expertise"/>
+  <section-skill :skill="skill"/>
+  <section-work :work="work"/>
+  <section-education :education="education"/>
 </div>
 </template>
-
 <script>
 import sectionHeader from '~/components/section-header.vue'
+import sectionCoding from '~/components/section-coding.vue'
+import sectionCodingHistory from '~/components/section-coding-history.vue'
+import sectionExpertise from '~/components/section-expertise.vue'
+import sectionSkill from '~/components/section-skill.vue'
+import sectionWork from '~/components/section-work.vue'
+import sectionEducation from '~/components/section-education.vue'
+
+const axios = require('axios')
 
 export default {
   components: {
-    sectionHeader
-    // about,
-    // skill,
-    // work
+    sectionHeader,
+    sectionCoding,
+    sectionCodingHistory,
+    sectionExpertise,
+    sectionSkill,
+    sectionWork,
+    sectionEducation
   },
-  dataAsync () {
-    return {
-    }
+  async asyncData () {
+    const { data } = await axios.get('/my-resume')
+    if (!data) throw new Error('Profile not found.')
+    return data
   }
 }
 </script>
