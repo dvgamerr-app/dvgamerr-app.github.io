2daeb182d1dc6797a8c7510438b9ad80e40eded2
Kananek T <info.dvgamer@gmail.com>
2019-01-21 00:59:12 +0700

    Create page-footer.vue

---
+++ components/page-footer.vue
@@ -0,0 +1,17 @@
+<template>
+<footer class="footer">
+  <div class="copyright-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copytext">TOUNO.io &copy; all rights reserved - Design By <a href="https://mr.touno.io" target="_blank">Kananek T.</a></div>
+        </div> 
+      </div>
+    </div>
+  </div>
+</footer>
+</template>
+<script>
+export default {
+}
+</script>
