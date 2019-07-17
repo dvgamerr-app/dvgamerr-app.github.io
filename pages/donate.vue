b64fbc5aab1132298dcf3bc29f901ccc6aa199e7
Kananek Thongkam <info.dvgamer@gmail.com>
2019-07-17 22:44:14 +0700

    images static fixed.

--- pages/donate.vue
+++ pages/donate.vue
@@ -64,7 +64,7 @@
           <div class="card mb-3">
             <div class="row no-gutters">
               <div class="col-md-4">
-                <img src="~assets/QR_SCB.jpg" class="card-img" alt="SCB">
+                <img src="/images/QR_SCB.jpg" class="card-img" alt="SCB">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
@@ -103,7 +103,7 @@
           <div class="card mb-3">
             <div class="row no-gutters">
               <div class="col-md-4">
-                <img src="~assets/QR_TRUE.jpg" class="card-img" alt="True Wallet">
+                <img src="/images/QR_TRUE.jpg" class="card-img" alt="True Wallet">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
