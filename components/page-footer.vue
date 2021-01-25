d722c2d950fac975ea074cf75058eab167675ba3
Kananek Thongkam <info.dvgamer@gmail.com>
2021-01-25 14:08:17 +0700

    add webring icon.

--- components/page-footer.vue
+++ components/page-footer.vue
@@ -6,7 +6,15 @@
           <div class="col-md-12">
             <div class="copytext">
               Copyright <fa :icon="['far', 'copyright']" /> {{ getYear }} {{ orgs }},  <br class="d-block d-sm-none"> all rights reserved ∙
-              Design By <a :href="url" target="_blank" v-text="name" />
+              Design By <a :href="url" target="_blank" v-text="name" />, and
+              <a href="https://webring.wonderful.software#mr.touno.io" title="วงแหวนเว็บ">
+                <img
+                  alt="วงแหวนเว็บ"
+                  width="14"
+                  height="14"
+                  src="https://webring.wonderful.software/webring.black.svg"
+                >
+              </a>
             </div>
           </div>
         </div>
