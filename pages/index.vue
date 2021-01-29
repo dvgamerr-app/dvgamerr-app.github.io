b2654648a835b63423479f02c35088fb1e2486e3
Kananek Thongkam <info.dvgamer@gmail.com>
2021-01-29 18:52:18 +0700

    fixed pagebreak in work.

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,33 @@
         <section-expertise :editor="allowEditor" :expertise="expertise" />
         <section-skill :editor="allowEditor" :skill="skill" />
         <section-education class="d-none print-only" :editor="allowEditor" :education="education" />
-        <p class="pagebreak" />
-        <section-work :editor="allowEditor" :work="work" />
-        <p class="pagebreak" />
+      </div>
+      <p class="pagebreak" />
+      <section v-for="e in work" :key="work.indexOf(e)" :class="e.pagebreak ? 'pagebreak' : 'section-wrapper section-work pt-3 pb-3'">
+        <div v-if="!e.pagebreak" class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <div class="section-title">
+                <h2>Work Experience</h2>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="content-item">
+                    <small v-text="toDateRange(e.range)" />
+                    <h3 v-text="e.job" />
+                    <h4 v-text="e.work" />
+                    <div class="markdown pt-1" v-html="markedown(e.description)" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <p class="pagebreak" />
+      <div v-if="!error">
         <section-education class="d-print-none" :editor="allowEditor" :education="education" />
         <section-certificate class="d-print-none" :editor="allowEditor" :certificate="certificate" />
         <section-portfolio class="d-print-none" :editor="allowEditor" :portfolio="portfolio" />
@@ -21,12 +45,15 @@
   </div>
 </template>
 <script>
+import marked from 'marked'
+import dayjs from 'dayjs'
+
 import sectionHeader from '~/components/section-header.vue'
 import sectionCoding from '~/components/section-coding.vue'
 import sectionCodingHistory from '~/components/section-coding-history.vue'
 import sectionExpertise from '~/components/section-expertise.vue'
 import sectionSkill from '~/components/section-skill.vue'
-import sectionWork from '~/components/section-work.vue'
+// import sectionWork from '~/components/section-work.vue'
 import sectionEducation from '~/components/section-education.vue'
 import sectionPortfolio from '~/components/section-portfolio.vue'
 import sectionCertificate from '~/components/section-certificate.vue'
@@ -40,7 +67,7 @@ export default {
     sectionCodingHistory,
     sectionExpertise,
     sectionSkill,
-    sectionWork,
+    // sectionWork,
     sectionEducation,
     sectionPortfolio,
     sectionCertificate,
@@ -92,6 +119,17 @@ export default {
   async created () {
   },
   methods: {
+    markedown (text) {
+      return marked(text)
+    },
+    toDateRange (range) {
+      const begin = dayjs(range.begin)
+      const end = range.end ? dayjs(range.end) : dayjs()
+      const diff = end.diff(begin, 'year', true)
+      const month = parseInt((diff - parseInt(diff)) * 12)
+      const year = parseInt(diff)
+      return `${begin.format('MMMM YYYY')} - ${range.end ? end.format('MMMM YYYY') : 'Present'} (${year > 0 ? `${year} year` : ''}${month > 0 ? ` ${month} month` : ''})`
+    },
     updateData (data = {}) {
       this.error = false
       this.fullname = data.fullname
