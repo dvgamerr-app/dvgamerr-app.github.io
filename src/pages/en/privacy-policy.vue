26b80f3d6be1780b5d1488987cae99efaf57e4f4
Kananek Thongkam <info.dvgamer@gmail.com>
2022-03-26 23:45:20 +0700

    Privacy Policy

---
+++ src/pages/en/privacy-policy.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Privacy Policy for Customer</h1>
+        <p>TOUNO.io recognizes the importance of the protection of your personal data. This Privacy Policy explains our practices regarding the collection, use or disclosure of personal data including other rights of the Data Subjects in accordance with the Personal Data Protection Laws.</p>
+
+        <h2>Collection of Personal Data</h2>
+        <p>
+        We will collect your personal data that receive directly from you as following:
+        </p><ul>
+        <li>email address</li>
+        </ul>
+        <p></p>
+
+        <h2>Types of Data Collected</h2>
+        <p><b>Technical data</b> such as IP address, Cookie ID, Activity Log, etc.</p>
+
+        <h2>Children</h2>
+        <p>If you are under the age of 20 or having legal restrictions, we may collect use or disclose your personal data. We require your parents or guardian to be aware and provide consent to us or allowed by applicable laws. If we become aware that we have collected personal data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
+
+        <h2>Storage of Data</h2>
+        <p>We store your personal data as hard copy and soft copy.</p>
+        <p>We store your personal data by using the following systems:</p>
+        <ul>
+        <li>Our server in Thailand</li>
+        <li>Our server outside of Thailand</li>
+        </ul>
+
+        <h2>Use of Data</h2>
+        <p>We use the collected data for various purposes:</p>
+        <ul>
+        <li>To comply with our Terms and Conditions</li>
+        <li>To comply with laws, rules, and regulatory authorities</li>
+        </ul>
+
+        <h2>Disclosure of Personal Data</h2>
+        <p>We may disclose your personal data to the following parties in certain circumstances:</p>
+
+        <p><b>Organization</b></p>
+        <p>We may disclose your personal data within our organization to provide and develop our products or services. We may combine information internally across the different products or services covered by this Privacy Policy to help us be more relevant and useful to you and others.</p>
+
+        <h2>Data Retention</h2>
+        <p>We will retain your personal data for as long as necessary during the period you are a customer or under relationship with us, or for as long as necessary in connection with the purposes set out in this Privacy Policy, unless law requires or permits a longer retention period. We will erase, destroy or anonymize your personal data when it is no longer necessary or when the period lapses.</p>
+
+        <h2>Data Subject Rights</h2>
+        <p>Subject to the Personal Data Protection Laws thereof, you may exercise any of these rights in the following:</p>
+
+        <p><b>Withdrawal of consent</b>: If you have given consent to us to collect, use or disclose your personal data whether before or after the effective date of the Personal Data Protection Laws, you have the right to withdraw such consent at any time throughout the period your personal data available to us, unless it is restricted by laws or you are still under beneficial contract.</p>
+
+        <p><b>Data access</b>: You have the right to access your personal data that is under our responsibility; to request us to make a copy of such data for you; and to request us to reveal as to how we obtain your personal data.</p>
+
+        <p><b>Data portability</b>: You have the right to obtain your personal data if we organize such personal data in automatic machine-readable or usable format and can be processed or disclosed by automatic means; to request us to send or transfer the personal data in such format directly to other data controllers if doable by automatic means; and to request to obtain the personal data in such format sent or transferred by us directly to other data controller unless not technically feasible.</p>
+
+        <p><b>Objection</b>: You have the right to object to collection, use or disclosure of your personal data at any time if such doing is conducted for legitimate interests of us, corporation or individual which is within your reasonable expectation; or for carrying out public tasks.</p>
+
+        <p><b>Data erasure or destruction</b>: You have the right to request us to erase, destroy or anonymize your personal data if you believe that the collection, use or disclosure of your personal data is against relevant laws; or retention of the data by us is no longer necessary in connection with related purposes under this Privacy Policy; or when you request to withdraw your consent or to object to the processing as earlier described.</p>
+
+        <p><b>Suspension</b>: You have the right to request us to suspend processing your personal data during the period where we examine your rectification or objection request; or when it is no longer necessary and we must erase or destroy your personal data pursuant to relevant laws but you instead request us to suspend the processing.</p>
+
+        <p><b>Rectification</b>: You have the right to rectify your personal data to be updated, complete and not misleading.</p>
+
+        <p><b>Complaint lodging</b>: You have the right to complain to competent authorities pursuant to relevant laws if you believe that the collection, use or disclosure of your personal data is violating or not in compliance with relevant laws.</p>
+
+        <p>You can exercise these rights as the Data Subject by contacting our Data Protection Officer as mentioned below. We will notify the result of your request within 30 days upon receipt of such request. If we deny the request, we will inform you of the reason via SMS, email address, telephone, registered mail (if applicable).</p>
+
+        <h2>Cookies</h2>
+        <p>To enrich and perfect your experience, we use cookies or similar technologies to display personalized content, appropriate advertising and store your preferences on your computer. We use cookies to identify and track visitors, their usage of our website and their website access preferences. If you do not wish to have cookies placed on your computer you should set their browsers to refuse cookies before using our website.</p>
+
+        <h2>Data Security</h2>
+        <p>We endeavor to protect your personal data by establishing security measures in accordance with the principles of confidentiality, integrity, and availability to prevent loss, unauthorized or unlawful access, destruction, use, alteration, or disclosure including administrative safeguard, technical safeguard, physical safeguard and access controls.</p>
+
+        <h2>Data Breach Notification</h2>
+        <p>We will notify the Office of the Personal Data Protection Committee without delay and, where feasible, within 72 hours after having become aware of it, unless such personal data breach is unlikely to result in a risk to the rights and freedoms of you. If the personal data breach is likely to result in a high risk to the rights and freedoms of you, we will also notify the personal data breach and the remedial measures to you without delay through our website, SMS, email address, telephone or registered mail (if applicable).</p>
+
+        <h2>Changes to this Privacy Policy</h2>
+        <p>We may change this Privacy Policy from time to time. Any changes of this Privacy Policy, we encourage you to frequently check on our website.</p>
+        <p>This Privacy Policy was last updated and effective on 26th March 2022</p>
+
+        <h2>Links to Other Sites</h2>
+        <p>The purpose of this Privacy Policy is to offer products or services and use of our website. Any websites from other domains found on our site is subject to their privacy policy which is not related to us.</p>
+
+        <h2>Contact Information</h2>
+        <p>If you have any questions about this Privacy Policy or would like to exercise your rights, you can contact us by using the following details:</p>
+
+        <p><b>Data Controller</b></p>
+        <p>TOUNO.io</p>
+
+        <p>
+        Charoenrat 3, Thung Wat Don, Sathon, Bangkok, 10120
+        </p>
+
+        <p>support@touno.io</p>
+        <p>www.touno.io</p>
+        <p>0270347607</p>
+
+        <p><b>Data Protection Officer</b></p>
+        <p>Policy Team</p>
+
+        <p>
+        Charoenrat 3, Thung Wat Don, Sathon, Bangkok, 10120
+        </p>
+
+        <p>support@touno.io</p>
+        <p>0270347607</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'social',
+  head () {
+    return {
+      title: 'Privacy Prolicy'
+    }
+  }
+}
+</script>
+
