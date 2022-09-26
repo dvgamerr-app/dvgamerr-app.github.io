2afc5f77f2084f03cc83e12d4b3def7fb5ec1415
Kananek Thongkam <info.dvgamer@gmail.com>
2022-09-26 16:40:38 +0700

    coding section updated.

---
+++ src/components/section-3-coding-history.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="section-wrapper section-coding d-print-none">
+    <div class="row">
+      <div class="col-lg-9 col-md-11">
+        <div class="section-title">
+          <h2>Code History</h2>
+        </div>
+      </div>
+      <div class="col-lg-27 col-md-25 col-sm-36 d-grid stats">
+        <div class="stats-item">
+          <div class="value" v-text="parseTime(coding.yesterday_seconds)" />
+          <div class="name">
+            Yesterday Coding
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="parseTime(coding.average_seconds)" />
+          <div class="name">
+            Daily Average
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="parseTime(coding.best_seconds)" />
+          <div class="name">
+            Best Coding
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="coding.languages" />
+          <div class="name">
+            languages
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import { coding } from '~/../docs/data/resume.json'
+
+export default {
+  data: () => ({ coding }),
+  methods: {
+    parseTime (seconds = 0) {
+      return `${seconds > 60 * 60 ? `${parseInt(seconds / 60 / 60)}h ` : ''}${parseInt(seconds / 60 % 60)}m`
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+.charts-css.column {
+  height: 240px;
+  max-width: 95%;
+  margin: 0 auto;
+}
+.charts-css.column > tbody td {
+  --color: var(--color-7);
+}
+
+</style>
---
+++ src/components/section-3-coding.vue
@@ -0,0 +1,59 @@
+<template>
+  <section v-if="!!coding" class="section-wrapper section-coding d-print-none">
+    <div class="row">
+      <div class="col-lg-9 col-md-11">
+        <div class="section-title">
+          <h2>My Coding</h2>
+        </div>
+      </div>
+      <div class="col-lg-27 col-md-25 col-sm-36 d-grid stats">
+        <div class="stats-item">
+          <div class="value" v-text="getExperience" />
+          <div class="name">
+            Work Experience
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="toNumber(coding.loc)" />
+          <div class="name">
+            Lines of Code
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="`${toNumber(coding.project)} (${toNumber(coding.opensource)})`" />
+          <div class="name">
+            Projects (opensource)
+          </div>
+        </div>
+        <div class="stats-item">
+          <div class="value" v-text="toNumber(coding.commits)" />
+          <div class="name">
+            Commits
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+<script>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { coding } from '~/../docs/data/resume.json'
+
+dayjs.extend(relativeTime)
+
+export default {
+  data: () => ({ coding }),
+  computed: {
+    getExperience () {
+      return dayjs(this.coding.experience).fromNow(true)
+    }
+  },
+  methods: {
+    toNumber (n = 0) {
+      return n / 1000000 < 1 && n / 1000 >= 1 ? `${parseInt(n / 1000 * 10) / 10}K` : (n / 1000000 >= 1 ? `${parseInt(n / 1000000 * 10) / 10}M` : n)
+    }
+  }
+}
+</script>
