f451b89f1d4fbb40155ee83e1329f72964696b6e
Kananek Thongkam <info.dvgamer@gmail.com>
2023-08-29 17:20:43 +0700

    refactor: layout and theme

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+  },
+  extends: [
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/no-v-html': 'off'
+  }
+}
