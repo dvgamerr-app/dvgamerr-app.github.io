8c16b6c027bcf1abf4b0c6a4c4de4676c9c73ca0
Kananek Thongkam <info.dvgamer@gmail.com>
2023-09-15 16:53:15 +0700

    refactor: update package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mr.touno.io',
-  integrations: [ image(), robotsTxt()]
+  integrations: [ robotsTxt() ],
+  experimental: { assets: true },
 });
