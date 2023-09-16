47b9fa0af3523b7e3496e64420e7181cee1a735f
Kananek Thongkam <info.dvgamer@gmail.com>
2023-09-16 18:58:00 +0700

    refactor: add ui button language

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: 'EN',
+  th: 'ไทย',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+  },
+  th: {
+    'nav.home': 'Accueil',
+  },
+} as const;
