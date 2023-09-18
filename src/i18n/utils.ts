af426337f78fd580b7dadd15a66a2a8decfd6fdc
Kananek Thongkam <info.dvgamer@gmail.com>
2023-09-18 12:00:01 +0700

    refactor: สanguage on header update

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,13 +1,11 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang } from './ui'
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
+  const [, lang] = url.pathname.split('/')
+  if (lang in ui) return lang as keyof typeof ui
+  return defaultLang
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  }
+  return (key: keyof typeof ui[typeof defaultLang]) => ui[lang][key] || ui[defaultLang][key]
 }
