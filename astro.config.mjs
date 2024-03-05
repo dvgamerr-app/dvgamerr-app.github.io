e30d8a8abf040c00a32726321e04104464bf0966
Kananek Thongkam <info.dvgamer@gmail.com>
2024-03-05 14:05:49 +0700

    refactor: add sitemap, web manifest, and internationalization (i18n) configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,32 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mr.touno.io',
-  integrations: [ robotsTxt() ],
+  site: 'https://dvgamerr.app',
+  integrations: [
+    robotsTxt(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          th: 'th-TH'
+        },
+      },
+    }),
+    webmanifest({
+      name: 'Kananek Thongkam | Full Stack Engineer',
+      icon: 'public/icon.webp', // source for favicon & icons
+      short_name: 'dvgamerr',
+      description: 'Hey there, My name is Kananek T. and I\'m a Full Stack Engineer.',
+      start_url: '/',
+      theme_color: '#3068d9',
+      background_color: '#f8f8f8',
+      display: 'standalone',
+    })
+  ]
 });
