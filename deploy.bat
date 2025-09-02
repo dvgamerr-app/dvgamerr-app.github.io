e28d4ee98dfd475a419adc00837a6fa7e42f05d3
Kananek Thongkam <info.dvgamer@gmail.com>
2025-09-02 12:22:33 +0700

    feat: add imstudioai/sim
    feat: add imstudioai/sim

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,10 @@
 @echo off
+
+bun collector/ai-blogs.js https://github.com/%*
 if "%~1"=="" (
   echo Commit message is required.
   exit /b 0
 )
 git add -A
-git commit -m "feat: %*"
+git commit -m "feat: add %*"
 git push
